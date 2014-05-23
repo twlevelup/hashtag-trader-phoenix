b8b87c5f61e3d051dc783a7db82d3da88d9acd6e
rabblerouser <danski@gmail.com>
2014-05-23 21:50:14 +1000

    Corrected tests to register as skipped with reason

--- test/models/user_details_test.rb
+++ test/models/user_details_test.rb
@@ -12,19 +12,24 @@ begin
  let(:user_name){"JohnSnow"}
 
   describe "when creating a user entry" do
-	xit "returns an exception if no first name is provided" do
+	it "returns an exception if no first name is provided" do
+		skip "Test must be re-written to use db correctly"
 		Proc.new {User_Details.new("", last_name, email, cat, user_name)}.must_raise ArgumentError		
 	end	
-	xit "returns an exception if no last name is provided" do
+	it "returns an exception if no last name is provided" do
+		skip "Test must be re-written to use db correctly"
 		Proc.new {User_Details.new(first_name, "", email, cat, user_name)}.must_raise ArgumentError			
 	end	
-	xit "returns an exception if no email is provided" do
+	it "returns an exception if no email is provided" do
+		skip "Test must be re-written to use db correctly"
 		Proc.new {User_Details.new(first_name, last_name, "", cat, user_name)}.must_raise ArgumentError				
 	end		
-	xit "returns an exception if no cat is provided" do
+	it "returns an exception if no cat is provided" do
+		skip "Test must be re-written to use db correctly"
 		Proc.new {User_Details.new(first_name, last_name, email, "", user_name)}.must_raise ArgumentError				
 	end	
-	xit "returns an exception if no user name is provided" do
+	it "returns an exception if no user name is provided" do
+		skip "Test must be re-written to use db correctly"
 		Proc.new {User_Details.new(first_name, last_name, email, cat, "")}.must_raise ArgumentError				
 	end	
   end
